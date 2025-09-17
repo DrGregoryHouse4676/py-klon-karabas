0d466cab5f9dc23433e851ce20c28f7ebaf13b7c
Lev <lev01111989@gmail.com>
2025-09-17 09:34:48 +0200

    added a little static

--- theatrebox/settings.py
+++ theatrebox/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'theatrebox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -167,3 +171,5 @@ SIMPLE_JWT = {
 }
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- theatrebox/urls.py
+++ theatrebox/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h1>🎭 TheatreBox</h1><p>Home page of the site</p>")
+
 
 urlpatterns = [
-path("admin/", admin.site.urls),
-path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
-path("api/", include("theatre.urls")),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
+    path("api/", include("theatre.urls")),
+    path("", index, name="index"),
 ]
